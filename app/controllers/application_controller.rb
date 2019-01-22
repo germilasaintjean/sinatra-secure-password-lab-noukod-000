5cdea367cb8ad07c946e756b109f23035e75d068
Saint-Jean Germila <germila@noukod.com>
2019-01-22 05:16:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,9 +43,9 @@ class ApplicationController < Sinatra::Base
   end
 	end
 
-	get "/success" do
+	get "/account" do
 		if logged_in?
-			erb :success
+			erb :account
 		else
 			redirect "/login"
 		end
