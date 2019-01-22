402866074d6ed5bc0f46e40acd10141457333e20
Saint-Jean Germila <germila@noukod.com>
2019-01-22 05:48:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,10 +30,10 @@ class ApplicationController < Sinatra::Base
 # 	get "/login" do
 # 		erb :login
 # 	end
-# 
+#
 #   post "/login" do
 #   user = User.find_by(:username => params[:username])
-# 
+#
 #   if user && user.authenticate(params[:password])
 #     session[:user_id] = user.id
 #     redirect "/signup"
