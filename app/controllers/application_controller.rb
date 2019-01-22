a91d00b855d646b7793a67b4a4fa4a8bf521e889
Saint-Jean Germila <germila@noukod.com>
2019-01-22 05:31:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,9 +37,9 @@ class ApplicationController < Sinatra::Base
 
   if user && user.authenticate(params[:password])
     session[:user_id] = user.id
-    redirect "/account"
+    redirect "/success"
   else
-     redirect "/success"
+     redirect "/failure"
    end
 	end
 
